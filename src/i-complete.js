73f95c2a6791fd3100a995e2e81f02a55e0a9b1d
Charles Lowell <cowboyd@frontside.io>
2016-04-27 18:49:07 -0400

    refactor towards attributes

--- src/i-complete.js
+++ src/i-complete.js
@@ -1,5 +1,5 @@
 class CompletionState {
-  constructor(attrs = {}) {
+  constructor(attrs) {
     Object.assign(this, attrs);
   }
 
@@ -12,9 +12,14 @@ class CompletionState {
   get isPending() { return false; }
 }
 
-export default class InitialState extends CompletionState {
-  constructor(query = '', value = null, reason = null) {
-    super({ query, value, reason, matches: [] });
+class InitialState extends CompletionState {
+  constructor(attrs = {}) {
+    super({
+      query: attrs.query || '',
+      value: attrs.value || null,
+      reason: attrs.reason || null,
+      matches: []
+    });
   }
 
   setQuery(query) {
@@ -23,18 +28,20 @@ export default class InitialState extends CompletionState {
 }
 
 class PendingState extends CompletionState {
-  constructor(prior, query) {
-    super({query, value: prior.value, prior});
+  constructor(initial, query) {
+    super({ query, initial });
   }
+
+  get value() { return this.initial.value; }
 
   get isPending() { return true; }
 
   setQuery(query) {
-    return new PendingState(this.prior, query);
+    return this.initial.setQuery(query);
   }
 
   cancel() {
-    return this.prior;
+    return this.initial;
   }
 
   resolve(results) {
@@ -49,15 +56,23 @@ class PendingState extends CompletionState {
   }
 
   reject(reason) {
-    return new InitialState(this.query, this.value, reason);
+    return new InitialState({
+      query: this.query,
+      value: this.value,
+      reason: reason
+    });
   }
 }
 
 class InspectingMatches extends CompletionState {
   constructor(pending, change) {
-    super({ query: pending.query, value: pending.value, pending });
+    super({ pending });
     Object.assign(this, change.call(this, this));
   }
+
+  get query() { return this.pending.query; }
+
+  get value() { return this.pending.value; }
 
   get isInspectingMatches() { return true; }
 
@@ -70,7 +85,10 @@ class InspectingMatches extends CompletionState {
   }
 
   select(match) {
-    return new InitialState(this.query, match.value);
+    return new InitialState({
+      query: this.query,
+      value: match.value
+    });
   }
 
   inspectNextMatch() {
@@ -189,271 +207,4 @@ export class NullMatch extends Match {
   }
 }
 
-// class InitialState {
-//   constructor() {
-//     Object.assign(this, {
-//       query: '',
-//       value: null,
-//       currentMatch: this.nullMatch,
-//       matches: [],
-//       isInspectingMatches: false,
-//       isPending: false,
-//       isFulfilled: false,
-//       isRejected: false,
-//       isSettled: false
-//     });
-//   }
-//   update(change) {
-//     let next = new State(this);
-//     change.call(null, next);
-//     return next;
-//   }
-
-//   get currentMatchIndex() {
-//     if (this.currentMatch) {
-//       return this.currentMatch.index;
-//     } else {
-//       return -1;
-//     }
-//   }
-
-//   get nullMatch() {
-//     return new NullMatch(this);
-//   }
-// };
-
-// class State extends InitialState {
-//   constructor(prev) {
-//     super();
-//     Object.assign(this, {
-//       query: prev.query,
-//       value: prev.value,
-//       currentMatch: prev.currentMatch,
-//       matches: prev.matches.slice(),
-//       isInspectingMatches: prev.isInspectingMatches,
-//       isPending: prev.isPending,
-//       isFulfilled: prev.isFulfilled,
-//       isRejected: prev.isRejected,
-//       isSettled: prev.isSettled
-//     });
-//   }
-// }
-
-// export class Match {
-//   constructor(state, attrs = {}, overrides = {}) {
-//     this.state = state;
-//     this.attrs = Object.assign({
-//       isCurrentMatch: false,
-//       isDefault: false,
-//       value: null
-//     }, attrs, overrides);
-//   }
-
-//   get isNull() { return false; }
-
-//   get isCurrentMatch() {
-//     return this.attrs.isCurrentMatch;
-//   }
-
-//   get isDefault() {
-//     return this.attrs.isDefault;
-//   }
-
-//   get index() {
-//     return this.attrs.index;
-//   }
-
-//   get value() {
-//     return this.attrs.value;
-//   }
-
-//   get previous() {
-//     if (this.index === 0) {
-//       return this.state.nullMatch;
-//     } else {
-//       return this.state.matches[this.index - 1];
-//     }
-//   }
-
-//   get next() {
-//     if (this.index === (this.state.matches.length - 1)) {
-//       return this.state.nullMatch;
-//     } else {
-//       return this.state.matches[this.index + 1];
-//     }
-//   }
-
-//   inspect(state) {
-//     return new Match(state, this.attrs, {
-//       isCurrentMatch: true
-//     });
-//   }
-
-//   uninspect(state) {
-//     return new Match(state, this.attrs, {
-//       isCurrentMatch: false
-//     });
-//   }
-// }
-
-// export class NullMatch extends Match {
-//   constructor(state, isCurrentMatch = false) {
-//     super(state, {
-//       isCurrentMatch: isCurrentMatch
-//     });
-//   }
-
-//   get isNull() { return true; }
-
-//   get index() { return -1; }
-
-//   get value()  { return null; }
-
-//   get previous() {
-//     if (this.state.matches.length === 0) {
-//       return this;
-//     } else {
-//       return this.state.matches[this.state.matches.length - 1];
-//     }
-//   }
-
-//   get next() {
-//     if (this.state.matches.length === 0) {
-//       return this;
-//     } else {
-//       return this.state.matches[0];
-//     }
-//   }
-
-//   inspect(state) {
-//     return new NullMatch(state, true);
-//   }
-
-//   uninspect(state) {
-//     return new NullMatch(state, false);
-//   }
-// }
-
-// export default class Recomplete {
-//   constructor(options = {}) {
-//     this.source = options.source || (()=> []);
-//     this.observe = options.observe || function() {};
-//     this.data = new InitialState();
-//     if (!options.defaultMatch || !options.defaultMatch.call) {
-//       this.defaultMatch = function() { return options.defaultMatch; };
-//     } else {
-//       this.defaultMatch = options.defaultMatch;
-//     }
-//   }
-
-//   update(change) {
-//     let next = this.data.update(function(next) {
-//       if (change.call) {
-//         change.call(null, next);
-//       } else {
-//         Object.assign(next, change);
-//       }
-//     });
-//     this.data = next;
-//     this.observe.call(null, next, this);
-//   }
-
-//   setQuery(query) {
-//     let value = this.source.call(this, query);
-//     let promise = value.then ? value : Promise.resolve(value);
-//     this.update({
-//       query: query,
-//       isPending: true,
-//       isRejected: false,
-//       isFulfilled: false,
-//       isSettled: false
-//     });
-//     let originalData = this.data;
-//     let updateIfFresh = (attrs)=> {
-//       if (originalData === this.data) {
-//         this.update(attrs);
-//       }
-//     };
-
-//     let defaultMatch = this.defaultMatch(query);
-
-//     return promise.then(function(result) {
-//       updateIfFresh(function(next) {
-//         next.isPending = false;
-//         next.isRejected = false;
-//         next.isFulfiled = true;
-//         next.isSettled = true;
-//         next.isInspectingMatches = !!result.length;
-//         next.currentMatch = next.nullMatch;
-//         next.matches = result.map((item, i)=> new Match(next, {
-//           index: i, value: item
-//         }));
-//         if (defaultMatch != null) {
-//           next.matches.push(new Match(next, {
-//             index: result.length,
-//             value: defaultMatch,
-//             isDefault: true
-//           }));
-//         }
-//       });
-//     }).catch(function(reason) {
-//       updateIfFresh({
-//         isPending: false,
-//         isRejected: true,
-//         isFulfilled: false,
-//         isSettled: true,
-//         reason: reason
-//       });
-//     });
-//   }
-
-//   cancel() {
-//     this.selectValue(this.data.value);
-//   }
-
-//   selectCurrentMatch() {
-//     this.selectValue(this.data.currentMatch);
-//   }
-
-//   selectValue(value) {
-//     return this.update({
-//       isPending: false,
-//       isRejected: false,
-//       isFulfilled: false,
-//       isSettled: false,
-//       value: value,
-//       isInspectingMatches: false,
-//       matches: []
-//     });
-//   }
-
-//   inspectNextMatch() {
-//     this.advanceCurrentMatch(1);
-//   }
-
-//   inspectPreviousMatch() {
-//     this.advanceCurrentMatch(-1);
-//   }
-
-//   advanceCurrentMatch(distance) {
-//     let nextMatch;
-//     let currentMatch = this.data.currentMatch;
-//     if (distance > 0) {
-//       nextMatch = currentMatch.next;
-//     } else {
-//       nextMatch = currentMatch.previous;
-//     }
-//     if (nextMatch !== currentMatch) {
-//       this.update(function (next) {
-//         next.currentMatch = nextMatch.inspect(next);
-//         next.matches = next.matches.map(function(match, i) {
-//           if (match.index === nextMatch.index) {
-//             return next.currentMatch;
-//           } else {
-//             return match.uninspect(next);
-//           }
-//         });
-//       });
-//     }
-//   }
-// };
+export default InitialState;
